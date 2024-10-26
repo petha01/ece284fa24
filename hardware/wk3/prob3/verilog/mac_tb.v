01201ddb47bcfe1069de8248a123693d62cbc692
Pethaperumal Natarajan <pnataraj@ucsd.edu>
2024-10-26 13:42:52 -0700

    incomplete prob 3

--- hardware/wk3/prob3/verilog/mac_tb.v
+++ hardware/wk3/prob3/verilog/mac_tb.v
@@ -21,8 +21,8 @@ integer w_scan_file ; // file handler
 integer x_file ; // file handler
 integer x_scan_file ; // file handler
 
-integer x_dec;
-integer w_dec;
+integer x_dec[3:0];
+integer w_dec[3:0];
 integer i; 
 integer u; 
 
@@ -115,12 +115,62 @@ function [psum_bw-1:0] mac_predicted;
 
 endfunction
 
+function [psum_bw-1:0] mac_predicted_4_input;
+  input [bw-1:0] a1;
+  input [bw-1:0] a2;
+  input [bw-1:0] a3;
+  input [bw-1:0] a4;
+  input [bw-1:0] w1;
+  input [bw-1:0] w2;
+  input [bw-1:0] w3;
+  input [bw-1:0] w4;
+  input [psum_bw-1:0] c;
+
+  reg unsigned [psum_bw-1:0] a_unsigned1;
+  reg unsigned [psum_bw-1:0] a_unsigned2;
+  reg unsigned [psum_bw-1:0] a_unsigned3;
+  reg unsigned [psum_bw-1:0] a_unsigned4;
+  reg signed [psum_bw-1:0] w_signed2;
+  reg signed [psum_bw-1:0] w_signed2;
+  reg signed [psum_bw-1:0] w_signed2;
+  reg signed [psum_bw-1:0] w_signed2;
+
+  reg [psum_bw-1:0] product1;
+  reg [psum_bw-1:0] product2;
+  reg [psum_bw-1:0] product3;
+  reg [psum_bw-1:0] product4;
+
+  begin
+    a_unsigned1 = $unsigned(a1);
+    a_unsigned2 = $unsigned(a2);
+    a_unsigned3 = $unsigned(a3);
+    a_unsigned4 = $unsigned(a4);
+    w_signed1 = $signed(w1);
+    w_signed2 = $signed(w2);
+    w_signed3 = $signed(w3);
+    w_signed4 = $signed(w4);
+
+    product1 = a_unsigned1 * w_signed1;
+    product2 = a_unsigned2 * w_signed2;
+    product3 = a_unsigned3 * w_signed3;
+    product4 = a_unsigned4 * w_signed4;
+    mac_predicted = c + product1 + product2 + product3 + product4;
+  end
+
+endfunction
+
 
 
 mac_wrapper #(.bw(bw), .psum_bw(psum_bw)) mac_wrapper_instance (
 	.clk(clk), 
-        .a(a), 
-        .b(b),
+        .a1(a1), 
+        .a2(a2), 
+        .a3(a3), 
+        .a4(a4), 
+        .b1(b1),
+        .b2(b2),
+        .b3(b3),
+        .b4(b4),
         .c(c),
 	.out(out)
 ); 
@@ -140,19 +190,27 @@ initial begin
 
   $display("-------------------- Computation start --------------------");
   
-  for (i=0; i<10; i=i+1) begin  // Data lenght is 10 in the data files
+  for (i=0; i<5; i=i+1) begin  // 20 numbers in file. 20/4 = 5
 
      #1 clk = 1'b1;
      #1 clk = 1'b0;
+     integer j;
+     for (j = 0; j<4; j=j+1) begin
+      w_scan_file = $fscanf(w_file, "%d\n", w_dec[j]);
+      x_scan_file = $fscanf(x_file, "%d\n", x_dec[j]);
+     end
 
-     w_scan_file = $fscanf(w_file, "%d\n", w_dec);
-     x_scan_file = $fscanf(x_file, "%d\n", x_dec);
-
-     a = x_bin(x_dec);
-     b = w_bin(w_dec);
+     a1 = x_bin(x_dec[0]);
+     a2 = x_bin(x_dec[1]);
+     a3 = x_bin(x_dec[2]);
+     a4 = x_bin(x_dec[3]);
+     b1 = w_bin(w_dec[0]);
+     b2 = w_bin(w_dec[1]);
+     b3 = w_bin(w_dec[2]);
+     b4 = w_bin(w_dec[3]);
      c = expected_out;
 
-     expected_out = mac_predicted(a, b, c);
+     expected_out = mac_predicted_4_input(a1, a2, a3, a4, b1, b2, b3, b4, c);
 
   end
 
--- hardware/wk3/prob3/verilog/mac_wrapper.v
+++ hardware/wk3/prob3/verilog/mac_wrapper.v
@@ -6,25 +6,74 @@ parameter bw = 4;
 parameter psum_bw = 16;
 
 output [psum_bw-1:0] out;
-input  [bw-1:0] a;
-input  [bw-1:0] b;
+input  [bw-1:0] a1;
+input  [bw-1:0] a2;
+input  [bw-1:0] a3;
+input  [bw-1:0] a4;
+input  [bw-1:0] b1;
+input  [bw-1:0] b2;
+input  [bw-1:0] b3;
+input  [bw-1:0] b4;
 input  [psum_bw-1:0] c;
 input  clk;
 
-reg    [bw-1:0] a_q;
-reg    [bw-1:0] b_q;
+reg    [bw-1:0] a1_q;
+reg    [bw-1:0] a2_q;
+reg    [bw-1:0] a3_q;
+reg    [bw-1:0] a4_q;
+
+reg    [bw-1:0] b1_q;
+reg    [bw-1:0] b2_q;
+reg    [bw-1:0] b3_q;
+reg    [bw-1:0] b3_q;
+
 reg    [psum_bw-1:0] c_q;
 
-mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance (
-        .a(a_q), 
-        .b(b_q),
+wire   [psum_bw-1:0] out1;
+wire   [psum_bw-1:0] out2;
+wire   [psum_bw-1:0] out2;
+
+mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance_1 (
+        .a(a1_q), 
+        .b(b1_q),
         .c(c_q),
-	.out(out)
+	.out(out1)
+);
+
+mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance_2 (
+        .a(a2_q), 
+        .b(b2_q),
+        .c(out1),
+	.out(out2)
 ); 
 
+mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance_3 (
+        .a(a3_q), 
+        .b(b3_q),
+        .c(0),
+	.out(out3)
+);
+
+mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance_4 (
+        .a(a4_q), 
+        .b(b4_q),
+        .c(out3),
+	.out(out4)
+); 
+
+assign out = out2 + out4;
+
 always @ (posedge clk) begin
-        b_q  <= b;
-        a_q  <= a;
+        b1_q  <= b1;
+        b2_q  <= b2;
+        b3_q  <= b3;
+        b4_q  <= b4;
+
+        a1_q  <= a1;
+        a2_q  <= a2;
+        a3_q  <= a3;
+        a4_q  <= a4;
+
         c_q  <= c;
 end
 
