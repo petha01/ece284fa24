b62bd9f5440dbb0647d791867e2ea503432e3c98
Pethaperumal Natarajan <nk.petha@gmail.com>
2024-10-26 15:34:56 -0700

    corrected mac

--- hardware/wk3/prob3/verilog/mac.v
+++ hardware/wk3/prob3/verilog/mac.v
@@ -11,8 +11,13 @@ input  signed [psum_bw-1:0] c;
 output [psum_bw-1:0] out;
 
 wire [psum_bw-1:0] product;
+wire [psum_bw-1:0] a_signed;
+wire [psum_bw-1:0] b_signed;
 
-assign product = $signed(a)*b;
+assign a_signed = $unsigned(a);
+assign b_signed = $signed(b);
+
+assign product = a_signed*b_signed;
 assign out = product + c;
 
 endmodule
